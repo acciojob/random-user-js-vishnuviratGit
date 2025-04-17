07fcb033b8ed0ff0e84fe559b0a90eccb515e06f
Vishnu V <vishnuvirat564@gmail.com>
2025-04-17 09:37:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 //your code here
+const API="https://randomuser.me/api/";
+ let userInfo={};
+ let ageBtn=document.getElementById("age");
+ let emailBtn=document.getElementById("email");
+ let phoneBtn=document.getElementById("phone");
+ let getUserBtn=document.getElementById("getUser");
+const fetchData= async ()=>{
+      try{
+          const response= await fetch(API);
+         const data=await response.json();
+           userInfo=data;
+           displayData(data);
+          console.log(data);
+          
+      }
+      catch(error){
+          console.log("error is:",error);
+      }
+}
+fetchData();
+function displayData(data){
+      document.getElementById("additionalInfo").innerHTML="";
+      document.getElementById("details").innerHTML=`
+          <img src="${data.results[0].picture.large}"/>
+          <h2>${data.results[0].name.first} ${data.results[0].name.last} </h2>
+          
+        `
+}
+ageBtn.addEventListener("click", ()=>{
+      document.getElementById("additionalInfo").innerHTML=`
+           <p>${userInfo.results[0].dob.age}</p>
+      `
+})
+emailBtn.addEventListener("click", ()=>{
+    document.getElementById("additionalInfo").innerHTML=`
+    <p>${userInfo.results[0].email}</p>
+`
+})
+phoneBtn.addEventListener("click", ()=>{
+    document.getElementById("additionalInfo").innerHTML=`
+    <p>${userInfo.results[0].phone}</p>
+`
+})
+getUserBtn.addEventListener("click", ()=>{
+     fetchData();
+})
